0ee967bf866dedac1adc6cd8b84c951651b82a34
Atharava <69344818+Atharava@users.noreply.github.com>
2021-08-18 15:11:08 +0530

    PlaceHolder change

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import FaceBookMsngr from './screens/fbm';
+import Instagram from './screens/inst';
+import WhatsAppMsngr from './screens/wta';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  render(){
+    return(
+        <AppContainer />
+    );
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  Facebook_Messenger: {screen: FaceBookMsngr},
+  Whatsapp: {screen: WhatsAppMsngr},
+  Instagram: {screen: Instagram},
+});
+
+const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+    alignItems: 'left',
+    justifyContent: 'left',
   },
+  placeHolder: {
+    color: "red"
+  }
 });
